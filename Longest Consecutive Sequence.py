c78f43f9d929b4045125309cbfd153ce4ba7a139
chaitanya <chaitanyajadaun@gmail.com>
2025-09-21 20:34:40 +0530

    longest consecutive sequennce (best)

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -1,3 +1,4 @@
+#self approach (o(nlogn))
 nums=sorted(set(nums))
 if len(nums)==0:
     return 0
@@ -11,3 +12,16 @@ for i in range(len(nums)-1):
     if count>ans:
         ans=count
 return ans
+
+#best approach (o(n))
+
+nums=set(nums)
+maxx=0
+for i in nums:
+    if i-1 not in nums:
+        count=1
+        while i+1 in nums:
+            count+=1
+            i+=1
+        maxx=max(count,maxx)
+return maxx
